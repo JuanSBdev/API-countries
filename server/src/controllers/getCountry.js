c84717fbed1feda710bf4e0c1c5ebb881fa51f81
Juan SB <juansb.dev@gmail.com>
2023-07-18 05:28:08 -0300

    db modelos

--- server/src/controllers/getCountry.js
+++ server/src/controllers/getCountry.js
@@ -1,10 +1,22 @@
 const URL = 'http://localhost:5000/countries'
 const axios = require('axios')
+const { Country, Activity } = require('../db')
 
 let getCountries = async (req, res)=>{
 try {
    const response = axios(URL)
    const data = response.data;
+    let pais = {
+        id : data.id,
+        nombre: data.name.official,
+        bandera: data.flags.png,
+        continente: data.continents,
+        capital: data.capital,
+        subregion: data.subregion,
+        poblacion:data.population,
+
+    }
+
    res.status(201).json(data)
    
 } catch (error) {
