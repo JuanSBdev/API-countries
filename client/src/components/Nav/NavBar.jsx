3cf6f259856bb0414fcb13afba23c0af06b3f84b
Juan SB <juansb.dev@gmail.com>
2023-07-20 18:14:11 -0300

    cambiemos

---
+++ client/src/components/Nav/NavBar.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import styles from './navBar.module.css'
+
+let logoWorld = 'https://cdn-icons-png.flaticon.com/512/4974/4974772.png'
+let logoBuscar = 'https://www.seekpng.com/png/full/958-9588716_buscar-buscarlogo-buscador-lupalogo-lupa-search-icon-search.png'
+export default function NavBar() {
+    let [pais, setPais] = useState('')
+
+    let handleInput = (e)=>{
+        let {value} = e.target
+        setPais(value)
+    }
+    let buscarPais = ()=>{
+
+    }
+
+    
+  return (
+    <div className={styles.wrapper}>
+        <div className={styles.divLogo}>
+            <img className={styles.imgLogo}
+            src={logoWorld} alt="" />
+        </div>
+        <div className={styles.divInput}>
+            <input onChange={handleInput}
+             type="text"
+             className={styles.inputText} />
+            <img src={logoBuscar}
+             alt="lupa"
+             onClick={buscarPais} />
+        </div>
+    </div>
+  )
+}
