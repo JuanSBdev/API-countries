e15fe71a911755caf33f79c174b29f9477beb951
Juan SB <juansb.dev@gmail.com>
2023-07-23 19:59:12 -0300

    fixes get

--- server/src/controllers/getCountries.js
+++ server/src/controllers/getCountries.js
@@ -6,32 +6,29 @@ let getCountries = async (req, res)=>{
 try {
    const response = await axios.get(URL)
    const data = response.data;
-   for(let i = 0; i < data.length; i++){
-       const country = data[i];
+   const pais = data.map((country)=> (
+       {
+                    id: country.cca3,
+                    nombre: country.name.common,
+                    bandera: country.flags.png,
+                    continente: country.continents,
+                    capital: country.capital ? country.capital[0] : 'None',
+                    subregion: country.subregion,
+                    area: country.area,
+                    poblacion: country.population,
+                  }
 
-       const capital = country.capital ? country.capital[0] : 'No encontró'; 
-        let subregion = country.subregion ? country.subregion : 'no existe';
-        let continente = country.continents ? country.continents[0] : 'no existe';
+   )
+   );
+   let paises = await Country.bulkCreate(pais)
+   res.status(200).json(paises)
 
-        const pais = {
-            id: country.cca3,
-            nombre: country.name.common,
-            bandera: country.flags.png,
-            continente: continente,
-            capital: capital,
-            subregion: subregion,
-            area: country.area,
-            poblacion: country.population,
-          };
-        
+   
 
-          let paises =  await Country.create(
-            pais)
-        }
-        res.status(201).json({ message: 'paises creados'})
    
 } catch (error) {
     res.status(400).json({error: error.message})
 }
 }
 module.exports = getCountries;
+
