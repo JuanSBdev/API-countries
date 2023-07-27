9994ebae2573f5919bd29a77bc1a88adb2674288
Juan SB <juansb.dev@gmail.com>
2023-07-27 02:00:43 -0300

    ft - estilos y start function

--- client/views/Start/Start.jsx
+++ client/views/Start/Start.jsx
@@ -9,18 +9,19 @@ export default function Start() {
   let dispatch = useDispatch()
   
   useEffect(()=>{
-    dispatch(getCountries())
     console.log('semanda')
   }, [])
   
   return (
     <div className={styles.wrapper}>
-      <h1 className={styles.h1_start}>Countries API</h1>
-      <Link to='/countries'>
-        <button className={styles.btn_start}>
-                          start
-        </button>
-      </Link>
+        <div className={styles.separador}>
+             <h1 className={styles.h1_start}>Countries API</h1>
+           <Link to='/countries' 
+           onClick={()=>dispatch(getCountries())}
+           className={styles.btn_start}>
+                               start
+           </Link>
+        </div>
         <p className={styles.p_start}>developed by juansb.dev@gmail.com</p>
     </div>
   )
