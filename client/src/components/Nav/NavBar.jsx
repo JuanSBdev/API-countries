de81129fe2a05c84062c2fa831a7eb278912c987
Juan SB <juansb.dev@gmail.com>
2023-07-28 18:53:36 -0300

    6to

--- client/src/components/Nav/NavBar.jsx
+++ client/src/components/Nav/NavBar.jsx
@@ -36,9 +36,10 @@ export default function NavBar() {
                  alt="lupa"
                  onClick={buscarPais} />
                 
-                
         </div>
-        <Link to={'/form'}>
+        <Link
+        className={styles.btn_activity}
+         to={'/form'}>
                     actividad
                 </Link>
     </div>
