a129e45754cb4d0760af1d0a754116b0902e4249
Juan SB <juansb.dev@gmail.com>
2023-07-31 23:37:24 -0300

    ft - details

--- client/views/form/FormActivity.jsx
+++ client/views/form/FormActivity.jsx
@@ -9,16 +9,19 @@ import Form from "../../src/components/activities/Form"
 export default function FormActivity(){
     return(
 
-        <div>
+<div>
+    <div className={styles.main}>
             <Form/>
             <Activities/>
-        <div>
-        <Link to={'/countries'}
-            className={styles.regresar}>
-            <img src="https://cdn-icons-png.flaticon.com/512/318/318493.png?w=360" alt="" />
-            <h4>BACK</h4> 
-        </Link>
-        </div>
+    </div>
+    <div className={styles.back}>
+    
+            <Link to={'/countries'}
+                className={styles.regresar}>
+                <img src="https://cdn-icons-png.flaticon.com/512/318/318493.png?w=360" alt="" />
+                <h4>BACK</h4> 
+            </Link>
+    </div>
 </div>
 
             )
