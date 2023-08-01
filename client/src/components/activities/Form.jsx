a5f24eb0eb64886c153246221fc920fd590a9ef5
Juan SB <juansb.dev@gmail.com>
2023-07-31 23:22:28 -0300

    formulario

---
+++ client/src/components/activities/Form.jsx
@@ -0,0 +1,124 @@
+import React, { useEffect, useState } from "react"
+import styles from './Form.module.css'
+import { Link } from "react-router-dom"
+import { useDispatch } from "react-redux"
+import { createActivity } from "../../redux/actions"
+
+export default function Form(){
+    let dispatch = useDispatch()
+
+    let [season, setSeason] = useState('spring')
+    let [countries, setCountries] = useState([])
+    let [form, setForm] = useState({
+        name:'',
+        places:'',
+        time:'',
+        season: season
+    })
+    let handleForm = (e)=>{
+        let {value, name} = e.target
+        e.preventDefault()
+        setForm((prevValues)=>({
+            ...prevValues,
+            [name]: value,
+
+        }))
+    }
+    let handleSelect = (e)=>{
+        e.preventDefault()
+        let {value} = e.target
+        setSeason(value)
+    }
+
+    let handleCountries = (e) => {
+        let { value } = e.target;
+            setCountries(value);
+
+    };
+    let addPlace = (e)=>{
+        // console.log(countries)
+        e.preventDefault()
+            if(countries.length > 0){
+            setForm((prevValues)=>({
+                ...prevValues,
+                places: [...prevValues.places, countries]
+    
+            }))
+
+        }
+        else{
+            alert('add a valid country for your activity')
+        }
+        setCountries('')
+    }
+
+    let removeCountry = (index) => {
+        const updatedCountries = form.places.filter((country, idx) => idx !== index);
+        setForm((prevValues) => ({
+            ...prevValues,
+            places: updatedCountries,
+        }));
+    };
+
+    let submitForm =(e)=>{
+        e.preventDefault()
+        // console.log(form)
+        dispatch(createActivity(form))        
+    }
+
+return(
+    <div className={styles.wrapper}>
+           
+            <h1 className={styles.h1}>Add Activity</h1>
+            <form action=""
+            onChange={handleForm}
+            onSubmit={submitForm}
+            className={styles.form}
+            >
+                <label >Name:</label>
+                <input type="text" name="name" />
+
+
+                <label > dificulty:</label>
+                <input type="number" id="numero" name="dificulty" min="1" max="5" placeholder="1-5" required />
+
+                <label >Duration:</label>
+                <input type="time" id="hora" name="time" min="1" max="24" required />
+
+                <label >Season:</label>
+                 <select name="season" id="season"
+                 onChange={handleSelect}>
+                      <option value="spring">Spring</option>
+                      <option value="summer">Summer</option>
+                      <option value="autumn">Autumn</option>
+                      <option value="winter">Winter</option>
+                </select>
+
+                <label >Place:</label>
+                <div className={styles.inputPlace}>
+                <input type="text" onChange={handleCountries} name="countries" id="countries"
+                value={countries}
+                 placeholder=""  maxLength="500"/>
+                <button onClick={addPlace}>+</button>
+                </div>
+                
+                <div className={styles.countriesSelected}>
+                    {form.places.length > 0 ? (
+                        form.places.map((country, index) => (
+                            <p key={index} onClick={()=>removeCountry(index)}>
+                                {country}
+                                </p>
+                        ))
+                    ) : (
+                        <p>'none'</p>
+                    )}
+                </div>
+
+                <button
+                className={styles.btn_submit}
+                 type="submit">Submit</button>
+
+            </form>
+    </div>
+)
+}
